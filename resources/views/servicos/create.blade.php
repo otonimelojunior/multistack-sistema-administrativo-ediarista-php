a0c1773b4bbc3dbfc14611d7305eec2d70d5dde6
OTONI MELO JUNIOR <OTONIMELOJUNIOR@GMAILCOM>
2021-09-04 13:34:48 -0300

    Add mascaras no projeto

--- resources/views/servicos/create.blade.php
+++ resources/views/servicos/create.blade.php
@@ -13,3 +13,7 @@
         @include('servicos._form')
     </form>
 @stop
+
+@section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.11.2/jquery.mask.min.js" integrity="sha512-Y/GIYsd+LaQm6bGysIClyez2HGCIN1yrs94wUrHoRAD5RSURkqqVQEU6mM51O90hqS80ABFTGtiDpSXd2O05nw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+@endsection
